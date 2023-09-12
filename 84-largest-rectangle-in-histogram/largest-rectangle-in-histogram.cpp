5c2c912d14614a75e9c57ce74d7e136e9206a5b6
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-09-12 12:58:35 +0530

    Time: 134 ms (73.51%) | Memory: 76.7 MB (80.65%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -8,76 +8,53 @@ public:
         int mxArea=0;
         int n=heights.size();
         stack<int>st;
-        vector<int>firstSmallerOnLeft(n,-1);
-        vector<int>firstSmallerOnRight(n,n);
 
-// first Smaller On Left computation. O(n) Time.
-        for(int i=0;i<n;i++)
+
+        for(int i=n-1;i>=0;i--)
         {
             if(st.size())
             {
                 int left=st.top();
-                if(heights[left]<heights[i])
+                if(heights[left]>heights[i])
                 {
-                    firstSmallerOnLeft[i]=left;
-                }
-                else
-                {
-                    while(st.size() and heights[st.top()]>=heights[i])
+                    while(st.size() and heights[st.top()]>heights[i])
                     {
+                        int height=heights[st.top()];
+                       
                         st.pop();
-                    }
-                    if(st.size())
-                    {
-                        firstSmallerOnLeft[i]=st.top();
+                        int area=0;
+                        if(st.size())
+                        {
+                            
+                            area=height*(st.top()-i-1);
+                        }
+                        else
+                        { 
+                            area=height*(n-i-1);
+                        }
+                        mxArea=max(mxArea, area);
+                        
                     }
                 }
             }
-
             st.push(i);
         }
-// clear the stack for the firstSmallerOnRight computation. O(n) Time.
+
         while(st.size())
         {
-            st.pop();
+          int height=heights[st.top()];
+          st.pop();
+          int area=0;
+                        if(st.size())
+                        {
+                            area=height*(st.top());
+                        }
+                        else
+                        {
+                            area=height*(n);
+                        }
+          mxArea=max(mxArea, area);   
         }
-
-
-// first Smaller On Right computation. O(n) Time.
-
-for(int i=n-1;i>=0;i--)
-        {
-            if(st.size())
-            {
-                int right=st.top();
-                if(heights[right]<heights[i])
-                {
-                    firstSmallerOnRight[i]=right;
-                }
-                else
-                {
-                    while(st.size() and heights[st.top()]>=heights[i])
-                    {
-                        st.pop();
-                    }
-                    if(st.size())
-                    {
-                        firstSmallerOnRight[i]=st.top();
-                    }
-                }
-            }
-
-            st.push(i);
-        }
-
-// Taking the max of the areas of max size rectangles with height equal to a perticular element in the array.
-
-      for(int i=0;i<n;i++)
-      {
-          int area=heights[i]*( firstSmallerOnRight[i]-firstSmallerOnLeft[i]-1);
-          mxArea=max(mxArea,area);
-      }
-
        
         return mxArea;
     }
