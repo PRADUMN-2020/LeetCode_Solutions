b4d969737c14fb84468d21a5a29d9fd7cb78f0a4
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-09-15 18:47:44 +0530

    Time: 63 ms (34.86%) | Memory: 24 MB (71.82%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
+        // Intuition: Here we declare a dp array of n,m where dp[i][j]: the no. of square submatrices ending at i,j. 
+        
+        //Here in we are using the matrix itself to save space.  
+        
+        // Here for the first row and column we keep it as it is. Now we iterate on each i,j where i is [1,n] and j is[1,m] and 
+        
+        // if matrix[i][j]==1 then martrix[i][j]=1+min(matrix[i-1][j],matrix[i-1][j-1],matrix[i][j-1])       [or matrix[i][j]+=min({matrix[i-1][j],matrix[i-1][j-1],matrix[i][j-1]})], 
+        
+        // else if martix[i][j] == 0, keep martix[i][j] 0 itself.
+
+        // Finally the total no. of square submatrices is the sum of the no. of square submatices ending at i,j for all i,j pairs.
+
         //Time:O(m*n) Space:O(1)
         int n=matrix.size();
         int m=matrix[0].size();
-        
+
         for(int i=1;i<n;i++)
         {
             for(int j=1;j<m;j++)
