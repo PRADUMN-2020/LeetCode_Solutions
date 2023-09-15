90f2f4fbb88d914064b1317ff5858932243f710f
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-09-15 20:08:06 +0530

    Time: 31 ms (19.86%) | Memory: 79.9 MB (7.24%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool f(int i,int j,int k,string &s1,string &s2,string &s3, int n,int m,int o, vector<vector<vector<int>>>&dp)
+    {
+        if(n-i+m-j!=o-k)
+        {
+            return 0;
+        }
+        if(i==n)
+        {
+            int ans=1;
+            for(;j<m;j++,k++)
+            {
+                if(s2[j]!=s3[k])
+                {
+                    ans=0;
+                    break;
+                }
+            }
+             return ans;
+        }
+
+        if(j==m)
+        {
+            int ans=1;
+            for(;i<n;i++,k++)
+            {
+                if(s1[i]!=s3[k])
+                {
+                    ans=0;
+                    break;
+                }
+            }
+             return ans;
+        }
+        if(dp[i][j][k]!=-1)
+        {
+            return dp[i][j][k];
+        }
+        if(s3[k]==s1[i] and s3[k]==s2[j])
+        {
+            return  dp[i][j][k]= f(i+1,j,k+1,s1,s2,s3,n,m,o,dp) or f(i,j+1,k+1,s1,s2,s3,n,m,o,dp);
+        }
+        else if(s3[k]==s1[i])
+        {
+           return  dp[i][j][k]=f(i+1,j,k+1,s1,s2,s3,n,m,o,dp);
+        }
+        else if(s3[k]==s2[j])
+        {
+            return  dp[i][j][k]= f(i,j+1,k+1,s1,s2,s3,n,m,o,dp);
+        }
+        else
+        {
+            return  dp[i][j][k]=0;
+        }
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int n=s1.size();
+        int m=s2.size();
+        int o=s3.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(o,-1)));
+        return f(0,0,0,s1,s2,s3,n,m,o,dp);
+    }
+};
