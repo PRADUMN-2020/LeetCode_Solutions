50aef77aab7ad65f36d808220accc7cd93f46e92
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-10-27 11:28:28 +0530

    Time: 8 ms (91.70%) | Memory: 7 MB (89.59%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mx=0,st=-1;
+    void evaluate(int l,int r,string &s)
+    {
+        int n=s.size();
+        while(l>=0 and r<n and s[l]==s[r])
+        {
+                if(mx<r-l+1)
+                {
+                   mx=r-l+1;
+                   st=l;
+                }
+                l--;
+                r++;
+        }
+    }
+    string longestPalindrome(string &s) {
+        int n=s.size();
+       for(int i=0;i<n;i++)
+       {
+           evaluate(i,i,s);
+           if(i+1<n)
+           evaluate(i,i+1,s);
+       }
+        string ans=s.substr(st,mx);
+    return ans;
+    }
+   
+};
