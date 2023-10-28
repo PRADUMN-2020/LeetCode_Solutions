76a2d4661317dc9f3f2e05fbd106eb8c4ae9c464
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-10-28 11:04:47 +0530

    Time: 14 ms (70.72%) | Memory: 6.4 MB (80.00%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int M=1e9+7;
     int countVowelPermutation(int n) {
-        vector<vector<long long>>dp(n+1,vector<long long>(5));
+        vector<long long>curr(5), prev(5);
         for(int i=0;i<5;i++)
         {
-            dp[1][i]=1;
+            prev[i]=1;
         }
         for(int i=2;i<=n;i++)
         {
@@ -13,30 +13,31 @@ public:
            {
                if(j==0)
                {
-                   dp[i][j]=(dp[i-1][1]+dp[i-1][2]+dp[i-1][4])%M;
+                   curr[j]=(prev[1]+prev[2]+prev[4])%M;
                }
                else if(j==1)
                {
-                   dp[i][j]=(dp[i-1][0]+dp[i-1][2])%M;
+                   curr[j]=(prev[0]+prev[2])%M;
                }
                else if(j==2)
                {
-                   dp[i][j]=(dp[i-1][1]+dp[i-1][3])%M;
+                  curr[j]=(prev[1]+prev[3])%M;
                }
                else if( j==3)
                {
-                   dp[i][j]=dp[i-1][2];
+                  curr[j]=prev[2];
                }
                else
                {
-                   dp[i][j]=(dp[i-1][2]+dp[i-1][3])%M;
+                  curr[j]=(prev[2]+prev[3])%M;
                }
            }
+           prev=curr;
         }
         long long ans=0;
         for(int i=0;i<5;i++)
         {
-             ans=(ans+dp[n][i])%M;
+             ans=(ans+prev[i])%M;
         }
         return ans;
     }
