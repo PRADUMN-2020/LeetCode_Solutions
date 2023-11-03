a5232a8adb04a98b16f8eb3795f473d8594f9b55
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-11-03 18:32:56 +0530

    Time: 105 ms (79.50%) | Memory: 20.3 MB (73.89%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
+        // Time:O(n*mlog(n*m)), Space:n*m logic: dijkstra algorithm.
         vector<int>deltaRow{1,-1,0,0};
         vector<int>deltaCol{0,0,1,-1};
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
@@ -9,7 +10,7 @@ public:
         vector<vector<int>>effort(n,vector<int>(m,INT_MAX));
         effort[0][0]=0;
         pq.push({0,{0,0}});
-        while(!(pq.top().second.first==n-1 and pq.top().second.second==m-1))
+        while(pq.size() and !(pq.top().second.first==n-1 and pq.top().second.second==m-1))
         {
            int eft=pq.top().first;
            int i=pq.top().second.first;
