d8a9016f19adb538d6dd94928cdaeedfa005764f
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-11-12 23:09:48 +0530

    Time: 66 ms (62.66%) | Memory: 33.2 MB (49.55%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int M=1e9+7;
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>>al(n);
+        for(auto it: roads)
+        {
+            al[it[0]].push_back({it[1],it[2]});
+            al[it[1]].push_back({it[0],it[2]});
+        }
+        vector<pair<long long,int>>dist(n,{1e18,0});
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        dist[0]={0,1};
+        pq.push({0,0});
+        while(pq.size())
+        {
+            auto it=pq.top();
+            int node=it.second;
+            long long cost=it.first;
+            pq.pop();
+            for(auto itr:al[node])
+            {
+                int adjNode=itr.first;
+                long long adjCost=itr.second;
+                if(cost+adjCost<dist[adjNode].first)
+                {
+                   dist[adjNode].first=cost+adjCost;
+                   dist[adjNode].second=0;
+                   dist[adjNode].second=(dist[adjNode].second+dist[node].second)%M;
+                   pq.push({dist[adjNode].first,adjNode});
+                }
+                else if (cost+adjCost==dist[adjNode].first)
+                {
+                    dist[adjNode].second=(dist[adjNode].second+dist[node].second)%M;
+                }
+            }
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     cout<<dist[i].first<<" "<<dist[i].second<<endl;
+        // }
+        return dist[n-1].second;
+        
+    }
+};
