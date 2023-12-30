6bd98989a286c5fcf4a49201a0da726640277fec
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-12-30 23:02:48 +0530

    Time: 340 ms (5.03%) | Memory: 78.6 MB (66.46%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-    // Time:O(n*log(n)), Space:O(n) where n is the size of arr and k is the no of distince elements in arr.
+    // Time:O(n*log(n)), Space:O(n) where n is the size of arr.
     int ans=0;
     unordered_map<int,int>mp;
     for(auto it:arr)
