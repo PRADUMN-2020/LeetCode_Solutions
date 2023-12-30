2c92aa422d352773bf342779dbc2f304665d134a
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-12-30 15:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (37.40%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int largestInteger(int num) {
+    // Time:O(n*log(n)), Space:O(5*n) here n is the number of digits in num
       priority_queue<int>oddpq,evenpq;
       vector<int>oddIndex,evenIndex;
       int n=0;
@@ -20,24 +21,24 @@ public:
           num/=10;
           n++;
       }
-        reverse(evenIndex.begin(),evenIndex.end());
-        reverse(oddIndex.begin(),oddIndex.end());
+       reverse(evenIndex.begin(),evenIndex.end());
+       reverse(oddIndex.begin(),oddIndex.end());
        vector<int>finalNo(n);
       for(int i=0;i<oddIndex.size();i++)
       {
           int oddmax=oddpq.top();
           oddpq.pop();
-          finalNo[n-1-oddIndex[i]]=oddmax;
+          finalNo[oddIndex[i]]=oddmax;
       }
         for(int i=0;i<evenIndex.size();i++)
       {
           int evenmax=evenpq.top();
           evenpq.pop();
-          finalNo[n-1-evenIndex[i]]=evenmax;
+          finalNo[evenIndex[i]]=evenmax;
       }
        long long ans=0;
        long long multiple=1;
-       for(int i=n-1;i>=0;i--)
+       for(int i=0;i<n;i++)
        {
          ans+=(multiple*finalNo[i]);
          multiple*=10;
