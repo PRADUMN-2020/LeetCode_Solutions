5f9395edce9f4a9295bc6603d2018b187f6899a5
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2024-02-26 12:02:38 +0530

    Time: 5 ms (24.89%) | Memory: 11.6 MB (48.09%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,47 +11,18 @@
  */
 class Solution {
 public:
-    void pre(TreeNode* root,vector<int>&preorder)
-    {
-        
-        if(root)
-        {
-            preorder.push_back(root->val);
-            pre(root->left,preorder);
-            pre(root->right,preorder);
-        }
-        else
-        {
-           preorder.push_back(-1);         
-        }
-    }
-    
-    
-    void in(TreeNode* root,vector<int>&inorder)
-    {
-        if(root)
-        {
-            in(root->left,inorder);
-            inorder.push_back(root->val);
-            in(root->right,inorder);
-        }
-        else
-        {
-            inorder.push_back(-1);
-        }
-    }
+    //Time:O(min(n,m)), Space:O(min(n,m))
     
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // Time:O(n+m), Space:O(n+m)
-        vector<int>inorder1;
-        vector<int>preorder1;
-        vector<int>inorder2;
-        vector<int>preorder2;
-        pre(p,preorder1);
-         pre(q,preorder2);
-        in(p,inorder1);
-        in(q,inorder2);
-        return inorder1==inorder2 and preorder1==preorder2;
+      if((p!=0)^(q!=0))
+      {
+          return 0;
+      }
+    if(p)
+    {
+        return p->val==q->val and isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
+    }
+    else return 1;
         
     }
 };
