94b230751224174d726d730d837501948cfc3b5d
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2024-02-26 14:03:03 +0530

    Time: 2369 ms (5.10%) | Memory: 419.6 MB (6.21%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    // dp[i][d]: longest arithmatic subsequence ending at i with common difference d.
-    int longestArithSeqLength(vector<int>& nums) {
-        // Time:O(n^2), Space:O(n*1000)
+    int longestArithSeqLength(vector<int>& nums) {  
         int n=nums.size();
         vector<unordered_map<int,int>>dp(n);
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int prev=0;prev<i;prev++)
-            {
-                int d=nums[i]-nums[prev];
-                dp[i][d]=max(dp[i][d], (dp[prev][d]==0?1:dp[prev][d])+1);
-                ans=max(ans,dp[i][d]);
-            }
-        }
-        return ans;
         
+        int ans=INT_MIN;
+      
+          for(int i=0;i<n;i++)
+          {
+              for(int j=0;j<i;j++)
+              {
+                     int d=nums[i]-nums[j];
+                     dp[i][d]=max(dp[i][d],(dp[j][d]==0?1:dp[j][d])+1);
+                     ans=max(ans,dp[i][d]);
+             }
+            
+          }
+      
+      return ans;
     }
 };
