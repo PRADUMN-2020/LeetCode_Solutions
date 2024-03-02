7962fd740519d9d17d0741597f8d1391f16fea73
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2024-03-02 22:19:56 +0530

    Time: 30 ms (28.88%) | Memory: 32.1 MB (8.75%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        // Time:O(n*log(n)), Space:O(n)
         priority_queue<int,vector<int>,greater<int>>pq;
         for(auto it: nums)
         {
