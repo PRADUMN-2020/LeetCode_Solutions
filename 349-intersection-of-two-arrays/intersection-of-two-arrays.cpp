d9b2275d72c510ce881144d8e7b8b7e16c5b1a32
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2024-03-10 11:13:56 +0530

    Time: 4 ms (68.61%) | Memory: 13.5 MB (15.40%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,34 +3,22 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         // Time:O(n*log(n)+m*log(m)), Space:O(1)
         vector<int>ans;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int i=0,j=0;
-        int n= nums1.size();
+        unordered_map<int,int>mp;
+        int n=nums1.size();
         int m=nums2.size();
-        while(i<n and j<m)
+        for(int i=0;i<n;i++)
         {
-            if(nums1[i]<nums2[j])
+            mp[nums1[i]]++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(mp.find(nums2[i])!=mp.end())
             {
-                i++;
-            }
-            else if(nums1[i]>nums2[j])
-            {
-                j++;
-            }
-            else
-            {
-                if(ans.size()==0)
-                ans.push_back(nums1[i]);
-                else
-                {
-                    if(ans[ans.size()-1]!=nums1[i])
-                    {
-                        ans.push_back(nums1[i]);
-                    }
-                }
-                i++;
-                j++;
+               if( mp[nums2[i]]!=-1)
+               {
+                   ans.push_back(nums2[i]);
+                   mp[nums2[i]]=-1;
+               }
             }
         }
         return ans;
